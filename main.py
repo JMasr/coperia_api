41bb34c07967cb999cc6fd3bec601d4ae557a8d2
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-11-22 18:00:12 +0100

    Refactor the main with new data and with the new constructor.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from src.data import UVigoPatient
 from src.util import Util
-
+from src.data import UVigoPatient
 
 if __name__ == "__main__":
     json_patient = {"resourceType": "Patient",
                     "id": "p001",
-                    "active": True,
+                    "gender": "male",
+                    "active": "True",
                     "name": [{"text": "Adam Smith"}],
                     "birthDate": "1985-06-12"
                     }
     json_observation = Util.read_json('data/observation_audio.json')
 
-    uvigo_pat = UVigoPatient(json_patient, json_observation)
+    uvigo_pat = UVigoPatient(json_patient, [json_observation, json_observation])
     print('Done!')
